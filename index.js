c2cf50dbdafbdefaecd1a30904f594079f451542
Mayank-Sisodiya19 <mayanksisodiya2000@gmail.com>
2024-03-09 23:49:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,95 @@ const userInfoContainer = document.querySelector(".user-info-container");
 
 //initially vairables need????
 
-let oldTab = userTab;
+let currentTab = userTab;
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
-oldTab.classList.add("current-tab");
+currentTab.classList.add("current-tab");
 getfromSessionStorage();
+
+
+
+function switchTab(clickedTab) {
+    if(clickedTab != currentTab) {
+        currentTab.classList.remove("current-tab");
+        currentTab = clickedTab;
+        currentTab.classList.add("current-tab");
+
+        if(!searchForm.classList.contains("active")) {
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+        }
+        else {
+            // main phlensearch waletab pe tha , ab your weather wale tab pe aa gya hoon
+            searchForm.classList.remove("actice");
+            userInfoContainer.classList.remove("active");
+            // ab mein your weather pe aa gya hoon toh weather bhi check krna padega so lets check local storage frst
+            getfromSessionStorage();
+        }
+    }
+}
+
+userTab.addEventListener("click", () => {
+    // pass clicked tab as input parameter
+    switchTab(userTab);
+});
+
+searchTab.addEventListener("click", () => {
+    // pass clicked tab as input parameter
+    switchTab(searchTab);
+});
+
+
+//check if cordinates are already present in session storage
+function getfromSessionStorage() {
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+
+    if(!localCoordinates) {
+        //agar local coordinates nahi mile
+        grantAccessContainer.classList.add("active");
+    }
+    else {
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+}
+
+async function fetchUserWeatherInfo(coordinates) {
+    const {lat, lon} = coordinates;
+    //make grantcontainer invisible
+    grantAccessContainer.classList.remove("active");
+    //make loader visible
+    loadingScreen.classList.add("active");
+
+    //API Call
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+          );
+        const  data = await response.json();
+
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(err) {
+        loadingScreen.classList.remove("active");
+        //HW
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
